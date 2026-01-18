6b9b4ca8995e7b06ce4d44a379c66c752f4c9ad5
Navodya Hewage <164496989+NavodyaHewage@users.noreply.github.com>
2026-01-18 21:02:15 +0530

    Set Up Backend Project Stature

---
+++ Wood_and_Funiture_Management_System_Backend/src/main/java/com/group_project/wfms_backend/WoodAndFunitureManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.group_project.wfms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodAndFunitureManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoodAndFunitureManagementSystemBackendApplication.class, args);
+    }
+
+}
---
+++ Wood_and_Funiture_Management_System_Backend/src/test/java/com/group_project/wfms_backend/WoodAndFunitureManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group_project.wfms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodAndFunitureManagementSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
