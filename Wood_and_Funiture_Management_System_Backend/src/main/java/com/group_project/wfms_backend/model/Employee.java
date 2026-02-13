6bf25bdb0b8182ed931cdb539d3a36bc31c5e4f8
sajini <sajinikaushalya910@gmail.com>
2026-02-13 08:30:41 +0530

    create entity(employee)
    create backend

---
+++ Wood_and_Funiture_Management_System_Backend/src/main/java/com/group_project/wfms_backend/model/Employee.java
@@ -0,0 +1,47 @@
+package com.group_project.wfms_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name ="Employee")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name ="Full_Name",nullable=false,length =200)
+    private String fullName;
+
+    @Column(name="Designation",length = 100)
+    private String designation;
+
+    @Column(name="Address",columnDefinition = "TEXT")
+    private String address;
+
+    @Column(name = "NIC", unique = true, length = 20)
+    private String nic;
+
+    @Column(name ="Mobile_Number",length =15)
+    private String mobileNumber;
+
+    @Column(name="Date_Joined")
+    private LocalDate dateJoined;
+
+    @Column(name="Is_active")
+    private Boolean isActive;
+
+    
+
+
+}
