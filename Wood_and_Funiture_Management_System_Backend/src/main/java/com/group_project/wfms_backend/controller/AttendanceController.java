a63cca69ad68464a7885c6f7706a13a6075df054
sajini <sajinikaushalya910@gmail.com>
2026-04-23 22:36:42 +0530

    attendance part complete
    all attendance part completed

--- Wood_and_Funiture_Management_System_Backend/src/main/java/com/group_project/wfms_backend/controller/AttendanceController.java
+++ Wood_and_Funiture_Management_System_Backend/src/main/java/com/group_project/wfms_backend/controller/AttendanceController.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/attendance")
+@RequestMapping("/attendance")
 @CrossOrigin(origins = "*")
 public class AttendanceController {
 
