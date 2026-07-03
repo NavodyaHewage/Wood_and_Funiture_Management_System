107e90cf3f581d73d5cf44bc1b7a76ae9c55e63c
Navodya Hewage <164496989+NavodyaHewage@users.noreply.github.com>
2026-07-04 04:53:20 +0530

    Environment Update

--- Wood-and-Funiture-Management-System-FrontEnd/src/app/environment/environment.prod.ts
+++ Wood-and-Funiture-Management-System-FrontEnd/src/app/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://YOUR-RAILWAY-BACKEND-URL.up.railway.app/api'  // Set after Step 6 (Railway domain generation)
+    apiUrl: 'https://wfms-backend-production.up.railway.app/api'  // Set after Step 6 (Railway domain generation)
 };
